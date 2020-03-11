49e4d58f2e844399f900ff56debcf59be2375d03
Alex Kashko <alexkrond@yahoo.com>
2020-03-11 16:56:38 +0300

    Add const file

---
+++ src/cst.js
@@ -0,0 +1,16 @@
+const CST = {
+  SCENES: {
+    LOADING: 'loading',
+    MAIN: 'main'
+  },
+  ENTITIES: {
+    TYPES: {
+      ANIMAL: 'animal',
+      PLANT: 'plant',
+      FOOD: 'food',
+      PRODUCT: 'product'
+    }
+  }
+};
+
+export default CST;
