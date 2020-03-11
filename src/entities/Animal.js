bfa00b885f3e8ca07c8f02da3481ee0b6a3c85f6
Alex Kashko <alexkrond@yahoo.com>
2020-03-11 22:00:38 +0300

    Add animal popup

--- src/entities/Animal.js
+++ src/entities/Animal.js
@@ -1,13 +1,18 @@
 import Phaser from 'phaser';
 import ProgressBar from "../components/ProgressBar.js";
+import config from "../config.js";
+import Button from "../components/Button.js";
 
 
 class Animal extends Phaser.GameObjects.Sprite {
-  constructor({ scene, x, y, texture, frame }) {
+  constructor({ scene, x, y, texture, frame, tile, entity }) {
     super(scene, x, y, texture, frame);
 
-    const scale = .8;
-    this.setScale(scale);
+    this.tile = tile;
+    this.entity = entity;
+
+    this.scale = .8;
+    this.setScale(this.scale);
 
     this.energy = 100;
     this.status = 'p';
@@ -15,19 +20,24 @@ class Animal extends Phaser.GameObjects.Sprite {
 
     this.progressBar = this.scene.add.existing(new ProgressBar({
       scene,
-      x: this.x - this.displayWidth / scale / 2,
-      y: this.y - this.displayHeight / scale / 2
+      x: this.x - this.displayWidth / this.scale / 2,
+      y: this.y - this.displayHeight / this.scale / 2
     }));
   }
 
-  feed(food) {
-    this.energy += food.energy;
-    this.energy = this.energy > 100 ? 100 : this.energy;
+  feed(foodId) {
+    if (this.scene.barn.getQuantity(foodId)) {
+      const food = config.entities.find(entity => entity.id === foodId);
+      this.scene.barn.take(foodId, 1);
+
+      this.energy += food.energy;
+      this.energy = this.energy > 100 ? 100 : this.energy;
+    }
   }
 
   update(deltaTime) {
     this.energy = this.energy > 0 ? this.energy - deltaTime : 0;
-    this.progress += deltaTime;
+    this.progress = this.progress + deltaTime * 10 > 100 ? 100 : this.progress + deltaTime * 10;
 
     this.progressBar.updateProgress(this.energy / 100);
   }
@@ -35,6 +45,81 @@ class Animal extends Phaser.GameObjects.Sprite {
   destroy(fromScene) {
     this.progressBar.destroy(fromScene);
     super.destroy(fromScene);
+  }
+
+  toBarn() {
+    this.scene.barn.put(this.entity.id, 1);
+    this.destroyPopup();
+    this.destroy();
+    this.tile.entity = null;
+  }
+
+  collectProduct() {
+    if (this.progress === 100) {
+      this.progress = 0;
+      this.scene.barn.put(this.entity.product, 1);
+    }
+  }
+
+  showPopup() {
+    this.popup = new Phaser.GameObjects.Container(this.scene, this.x, this.y - this.displayHeight / this.scale / 2);
+
+    let i = 0;
+
+    const toBarnBtn = new Button({
+      scene: this.scene,
+      x: 0,
+      y: -i * config.textLineHeight,
+      text: 'to barn',
+      style: {
+        backgroundColor: '#ffd260'
+      },
+      clickCallBack: () => this.toBarn()
+    });
+    toBarnBtn.setOrigin(0.5, 1);
+    this.popup.add(toBarnBtn);
+    i++;
+
+
+    if (this.scene.barn.getQuantity(this.entity.eat)) {
+      const feedBtn = new Button({
+        scene: this.scene,
+        x: 0,
+        y: -i * config.textLineHeight,
+        text: 'feed',
+        style: {
+          backgroundColor: '#ffd260'
+        },
+        clickCallBack: () => this.feed(this.entity.eat)
+      });
+      feedBtn.setOrigin(0.5, 1);
+      this.popup.add(feedBtn);
+      i++;
+    }
+
+
+    if (this.progress === 100) {
+      const collectBtn = new Button({
+        scene: this.scene,
+        x: 0,
+        y: -i * config.textLineHeight,
+        text: 'collect',
+        style: {
+          backgroundColor: '#ffd260'
+        },
+        clickCallBack: () => this.collectProduct()
+      });
+      collectBtn.setOrigin(0.5, 1);
+      this.popup.add(collectBtn);
+    }
+
+
+    this.scene.add.existing(this.popup);
+  }
+
+  destroyPopup() {
+    this.popup.destroy();
+    this.popup = null;
   }
 }
 
